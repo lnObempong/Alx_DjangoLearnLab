70041751c204a8a7e182075741963f8e85fb3aa7
Leonard Nketia Obempong <lnobempong@gmail.com>
2025-09-10 13:12:13 +0000

    Add all

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,14 +1,6 @@
-# bookshelf/forms.py
 from django import forms
-from .models import Book
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ["title", "author", "published_date", "isbn", "category"]
-
-    def clean_isbn(self):
-        isbn = self.cleaned_data.get("isbn")
-        if isbn and not isbn.isdigit():
-            raise forms.ValidationError("ISBN must contain only digits.")
-        return isbn
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
