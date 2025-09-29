33a81deb8352cdf73e6e270eef69530b734a777e
Leonard Nketia Obempong <lnobempong@gmail.com>
2025-09-29 13:52:59 +0000

    Added

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -46,3 +46,27 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["content"]
+
+from django import forms
+from taggit.forms import TagWidget
+from .models import Post, Comment
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ["title", "content", "tags"]
+        widgets = {
+            "tags": TagWidget(attrs={"placeholder": "Add tags separated by commas"}),
+        }
+
+
+class CommentForm(forms.ModelForm):
+    content = forms.CharField(
+        widget=forms.Textarea(attrs={'rows': 3, 'placeholder': 'Write a comment...'}),
+        max_length=2000,
+        label=''
+    )
+
+    class Meta:
+        model = Comment
+        fields = ["content"]
