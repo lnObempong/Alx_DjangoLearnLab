187b3cc792e274a4cc772a85df958b8a15c3c674
Leonard Nketia Obempong <lnobempong@gmail.com>
2025-09-29 14:13:45 +0000

    Added

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
@@ -15,7 +16,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    tags = TaggableManager() 
+    published_date = models.DateTimeField(null=True, blank=True)  # ✅ Added
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
