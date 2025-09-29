58faf9bb622ddf927fe5d087aebbc583d7b317fb
Leonard Nketia Obempong <lnobempong@gmail.com>
2025-09-29 12:14:41 +0000

    Database updated

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3', 
+        'NAME': BASE_DIR / "db.sqlite3",
+        'USER': '',       
+        'PASSWORD': '',  
+        'HOST': '',       
+        'PORT': '',       
     }
 }
 
