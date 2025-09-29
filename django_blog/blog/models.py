f5db194f41ca8d88b9748d5760a97df7b4e87a19
Leonard Nketia Obempong <lnobempong@gmail.com>
2025-09-29 13:50:47 +0000

    Added tagging and search

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -14,7 +15,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    tags = models.ManyToManyField(Tag, related_name="posts", blank=True)
+    tags = TaggableManager() 
 
     def __str__(self):
         return self.title
